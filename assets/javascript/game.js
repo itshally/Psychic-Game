78bcbc22186ddc92b0b5f4976e96f7b5b14c887a
HB <hbianan@gmail.com>
2019-07-05 20:57:41 -0400

    update testing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,30 +16,54 @@ var winCount = document.getElementById('win-count'),
     winCount.textContent = Number(win);
     loseCount.textContent = Number(lose);
     guessLeft.textContent = Number(guess);
-                
-
+          
 // function that generates letters
-function generateLetter() {
-    // console.log(letters[Math.floor(Math.random() * letters.length)]);
-     return letters[Math.floor(Math.random() * letters.length)];
+function generateLetter(){
+    let letter = letters[Math.floor(Math.random() * letters.length)];
+    console.log(letter);
+    return letter;
 }
 
-var x = generateLetter();
+var newLetter = generateLetter();
 
-// console.log(generateLetter());
+//function that restarts the game whether the user lost or won
+function restart(){
+    guess = 9;
+    typedLetters.innerHTML = "";
+    guessLeft.textContent = Number(guess);
+    newLetter = generateLetter();
+    return newLetter;
+}
+
 document.onkeypress = function(e) {
     //key must be alphabets only
     if((e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode >= 97 && e.keyCode <= 122)){
         console.log("letter");
+        //if key is not match to the generated letter
+        var coded = String.fromCharCode(e.keyCode).toLowerCase();
+        if(coded !== newLetter){
+            //list the pressed letters
+            console.log("NOT MATCHED");
+            typedLetters.innerHTML += e.key.toLowerCase()  + ", ";
+
+            //if guess left is 0
+            if(guess == 0){
+                lose += 1;
+                loseCount.textContent = lose;
+                restart();
+            }else{
+                guess--;
+                guessLeft.textContent = guess;
+            }
+        }else{
+            console.log("MATCHED");
+            win += 1;
+            winCount.textContent = win;
+            restart();
+        }
     }else{
         console.log("ERROR: You must enter letters only");
     }
     
 }//end of onkeypress
 
-//reset function
-function restart(){
-    guess = 9;
-    typedLetters.innerHTML = "";
-    guessLeft.textContent = Number(guess);
-}
