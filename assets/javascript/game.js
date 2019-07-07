1919c74ba178a37b7ca3edebb5b7a07acc571a8b
HB <hbianan@gmail.com>
2019-07-06 23:12:19 -0400

    update game.js
    - changed syntax
    - fixed functions
    - added comments
    - did testing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,68 +1,113 @@
 //declaring variables
-var winCount = document.getElementById('win-count'),
-    loseCount = document.getElementById('lose-count'),
-    guessLeft = document.getElementById('guess-left'),
+var winCounter = document.getElementById('win-count'),
+    loseCounter = document.getElementById('lose-count'),
+    guessCounter = document.getElementById('guess-left'),
     typedLetters = document.getElementById('typed-letters'),
+    greenAlert = document.getElementById('green-box'),
+    redAlert = document.getElementById('red-box'),
     
     //default values
-    win = 0,
-    lose = 0,
-    guess = 9,
+    winPoint = 0,
+    losePoint = 0,
+    guessesLeft = 10,
     letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
                 "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", 
                 "w", "x", "y", "z"];
+    winCounter.textContent = Number(winPoint);
+    loseCounter.textContent = Number(losePoint);
+    guessCounter.textContent = Number(guessesLeft);
 
-    
-    winCount.textContent = Number(win);
-    loseCount.textContent = Number(lose);
-    guessLeft.textContent = Number(guess);
           
-// function that generates letters
+//a function that generates letters
 function generateLetter(){
     let letter = letters[Math.floor(Math.random() * letters.length)];
-    console.log(letter);
     return letter;
 }
 
+//a variable with a value of function
 var newLetter = generateLetter();
 
-//function that restarts the game whether the user lost or won
+//a function that restarts the game after the user lost or won
 function restart(){
-    guess = 9;
+    guessesLeft = 10;
     typedLetters.innerHTML = "";
-    guessLeft.textContent = Number(guess);
+    guessCounter.textContent = Number(guessesLeft);
     newLetter = generateLetter();
     return newLetter;
 }
 
+//getting the pressed key event on the page
 document.onkeypress = function(e) {
-    //key must be alphabets only
+    //hides the alert box by default
+    $('#green-box').hide();
+    $('#red-box').hide();
+    
+    //a conditional statement that checks if the keyCode is for a letter or not
     if((e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode >= 97 && e.keyCode <= 122)){
-        console.log("letter");
-        //if key is not match to the generated letter
-        var coded = String.fromCharCode(e.keyCode).toLowerCase();
-        if(coded !== newLetter){
-            //list the pressed letters
-            console.log("NOT MATCHED");
-            typedLetters.innerHTML += e.key.toLowerCase()  + ", ";
+        
+        //a variable that converts the keyCode number to its string value
+        var keyPressed = String.fromCharCode(e.keyCode).toLowerCase();
 
-            //if guess left is 0
-            if(guess == 0){
-                lose += 1;
-                loseCount.textContent = lose;
+        //checks if the pressed key is not matched to the computer generated letter
+        if(keyPressed !== newLetter){
+
+            //if the guess counter runs out
+            if(guessesLeft == 1){
+
+                //an alert-danger box will appear with a string that shows the correct letter
+                $('#red-box').show();
+                redAlert.textContent = "You lost! It's letter " + newLetter;
+                
+                //counter for lose points will increment to 1
+                losePoint += 1;
+                loseCounter.textContent = losePoint;
+
+                //then the game will restart again
                 restart();
-            }else{
-                guess--;
-                guessLeft.textContent = guess;
+
+            //however, if there are still guesses left    
+            }else
+            {   
+                //and if the letter hasn't been pressed, it will add up to the list
+                if(!typedLetters.innerHTML.match(keyPressed)){
+                    //shows the list of pressed letters; automatically changes to lowercase...
+                    //if user entered a capital letter
+                    typedLetters.innerHTML += e.key.toLowerCase()  + ", ";
+                    
+                    //guess counter will keep on decrementing
+                    guessesLeft--;
+                    guessCounter.textContent = guessesLeft;
+
+                //but if the letter has already been pressed, it  will genereate an alert box
+                //and guess counter will not going to decrement
+                }else{
+                    $('#red-box').show();
+                    redAlert.textContent = "You already pressed it.";
+                }
             }
+
+        //if the pressed key is matched to the computer generated letter
         }else{
-            console.log("MATCHED");
-            win += 1;
-            winCount.textContent = win;
+            
+            //an alert-success box will appear with a string that shows achievement
+            $('#green-box').show();
+            greenAlert.textContent = "You got it! It's letter " + newLetter;
+
+            //counter for win points will inrement to 1
+            winPoint += 1;
+            winCounter.textContent = winPoint;
+
+            //then the game will restart again
             restart();
+            
         }
+    
+    //if the user pressed any key that is not included in the conditional statement
     }else{
-        console.log("ERROR: You must enter letters only");
+
+        //an alert-danger box will appear with a string
+        $('#red-box').show();
+        redAlert.textContent = "Letters Only";
     }
     
 }//end of onkeypress
